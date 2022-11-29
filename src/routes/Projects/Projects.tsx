b92b0221ba204b7c675e10ec027ad6fc07630bf2
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-11-29 11:24:19 -0500

    add projects page

---
+++ src/routes/Projects/Projects.tsx
@@ -0,0 +1,14 @@
+import Hero from "../../Components/Hero";
+import devImg from "../../Assets/Hero/programming.jpeg";
+
+const Projects = () => {
+  return (
+    <div>
+      <Hero image={devImg}>
+        <h1>Projects</h1>
+      </Hero>
+    </div>
+  );
+};
+
+export default Projects;
