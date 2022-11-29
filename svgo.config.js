384b986b5680fac81f817422338ff4be5a4ee682
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-11-29 11:00:07 -0500

    add svgo config

---
+++ svgo.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    {
+      name: "prefixIds",
+      active: true,
+    },
+    {
+      name: "removeViewBox",
+      active: false,
+    },
+    {
+      name: "removeDimensions",
+      active: true,
+    },
+  ],
+};
